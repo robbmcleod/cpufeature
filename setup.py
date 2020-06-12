5d8bf6aa8622027c843bebd1f974b4b5759a0ffe
Widget_An <anchunyu@heywhale.com>
2020-06-12 16:31:15 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -37,7 +37,6 @@ LFLAGS = os.environ.get('LFLAGS', '').split()
 # Sources and headers
 sources = [  
     'cpufeature/cpu_x86.c', 'cpufeature/module.c'
-    # 'cpufeature/module.c'
 ]
 inc_dirs = ['cpufeature',]
 lib_dirs = []
