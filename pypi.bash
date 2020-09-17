254cc6129e87b5bbc6f3dfc60dbff774abb22a16
Robert McLeod <robbmcleod@gmail.com>
2020-09-16 18:56:23 -0700

    Update upload script to use `twine`

--- pypi.bash
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# SDIST
-python setup.py sdist bdist_wheel upload -r pypi
-# Apparently one should use twine now:
-# https://packaging.python.org/tutorials/distributing-packages/#packaging-your-project
---
+++ upload2pypi.bat
@@ -0,0 +1,2 @@
+python setup.py sdist
+twine upload dist/cpufeature-0.1.1.tar.gz
